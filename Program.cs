3a02e5c2371a32e20a966ce11bf116cabff7b663
ZingaG <trad_bar@mail.ru>
2023-03-07 19:34:08 +0300

    Palindrom function added and Task 1 completed

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@
     int result = Int32.Parse(System.Console.ReadLine());
     return result;
 }
+
+bool Palindrom(int a)
+{
+    int lastDigit = a % 10;
+    a = a / 10000;
+    int firstDigit = a % 10;
+    return (lastDigit == firstDigit);
+}
+
+// Task 1
+
+Console.WriteLine(Palindrom(TakeDigit("Enter a = ")));
