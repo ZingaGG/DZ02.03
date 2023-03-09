36ce0f48f77040cdcad1b68416f53394927799f0
ZingaG <trad_bar@mail.ru>
2023-03-09 12:07:00 +0300

    ArrayCoord and DistanceAtoB Functions added, Task 2 completed

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
     return result;
 }
 
+double TakeDigitDouble(string a)
+{
+    System.Console.WriteLine(a);
+    double result = Double.Parse(System.Console.ReadLine());
+    return result;
+}
+
+
 bool Palindrom(int a)
 {
     int lastDigit = a % 10;
@@ -13,6 +21,32 @@ bool Palindrom(int a)
     return (lastDigit == firstDigit);
 }
 
-// Task 1
+double[] ArrayCoord(double[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = TakeDigitDouble($"Input {i+1} coordinate");
+    }
+    return a;
+}
 
+double DistanceAtoB(double[] a, double[] b)
+{
+    double result = Math.Sqrt(Math.Pow(b[0]-a[0],2)+Math.Pow(b[1]-a[1],2)+Math.Pow(b[2]-a[2],2));
+    return result;
+}
+
+// Task 1
+/*
 Console.WriteLine(Palindrom(TakeDigit("Enter a = ")));
+*/
+// Task 2
+
+double[] FirstPoint = new double[3];
+ArrayCoord(FirstPoint);
+
+double[] SecondPoint = new double[3];
+ArrayCoord(SecondPoint);
+
+System.Console.WriteLine("Distance = " + DistanceAtoB(FirstPoint, SecondPoint));
+
